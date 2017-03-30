1c01df8ab5e8fc7513731c180f178eca0e07d67c
klimczyk.krzysztof <klimczyk.krzysztof@gmail.com>
2017-03-30 22:16:38 +1100

    Cleaned up Camera struct

--- include/Camera.h
+++ include/Camera.h
@@ -5,27 +5,27 @@
 #include "Object3d.h"
 #include "Ray.h"
 
-class Camera : public Object3d
+struct Camera : public Object3d
 {
-public:
-    Camera(const vec3 &pos, const vec3 &lookAt, const vec3 &upV, float fovy, float aspectRatio, float focus_dist);
+    vec3 u, v, w; // coordinate frame
+    vec3 position;
+    vec3 lookAt;
+    vec3 upVector;
+    float aperature;
+    float resX;
+    float resY;
+    vec3 topLeftCorner;
+    vec3 horizontalVector;
+    vec3 verticalVector;
+    float fov;
+
+    Camera(const vec3 &pos, const vec3 &lookAt, const vec3 &upV,
+           float fovy, float aspectRatio, float focus_dist);
 
     Ray GetRay(float u, float v) const;
 
     ~Camera();
 
-private:
-    vec3 m_Pos;
-    vec3 m_LookAt;
-    vec3 m_UpVector;
-    float aperature;
-    float m_resX;
-    float m_resY;
-    vec3 m_TopLeftCorner;
-    vec3 m_HorizontalVector;
-    vec3 m_VerticalVector;
-    float m_Fov;
-    vec3 u, v, w;
 };
 
 #endif // CAMERAH
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -13,26 +13,27 @@ vec3 random_in_unit_disc() {
 }
 
 
-Camera::Camera(const vec3 & pos, const vec3 & lookAt, const vec3 & upV, float fov, float aspectRatio, float focus_dist) :
-    m_Pos(pos),
-    m_LookAt(lookAt),
-    m_UpVector(upV),
-    m_Fov(fov),
+Camera::Camera(const vec3 & pos, const vec3 & lookAt, const vec3 & upV,
+        float fov, float aspectRatio, float focus_dist) :
+    position(pos),
+    lookAt(lookAt),
+    upVector(upV),
+    fov(fov),
     aperature(0.f)
 {
     // Create coordinate frame
-    w = vec3::normalize(m_Pos - m_LookAt);
-    u = vec3::normalize(vec3::cross(m_UpVector, w));
+    w = vec3::normalize(position - lookAt);
+    u = vec3::normalize(vec3::cross(upVector, w));
     v = vec3::cross(w, u);
 
     float theta = fov * M_PI / 180.0f; // convert to radians
     float half_height = tan(theta / 2);
     float half_width = aspectRatio * half_height;
 
-    m_HorizontalVector = 2 * half_width * u * focus_dist;
-    m_VerticalVector = 2 * half_height * v * focus_dist;
-    m_TopLeftCorner = (m_Pos - w - half_width * u * focus_dist - half_height *
-                       v * focus_dist - focus_dist * w);
+    horizontalVector = 2 * half_width * u * focus_dist;
+    verticalVector = 2 * half_height * v * focus_dist;
+    topLeftCorner = (position - w - half_width * u * focus_dist -
+                     half_height * v * focus_dist - focus_dist * w);
 }
 
 Ray Camera::GetRay(float s, float t) const
@@ -40,7 +41,8 @@ Ray Camera::GetRay(float s, float t) const
     // aperature
     vec3 rd = vec3(this->aperature) * random_in_unit_disc();
     vec3 offset = u * rd.x() + v * rd.y();
-    return Ray(m_Pos + offset, m_TopLeftCorner + s * m_HorizontalVector + t * m_VerticalVector - m_Pos - offset);
+    return Ray(position + offset, topLeftCorner + s * horizontalVector +
+               t * verticalVector - position - offset);
 }
 
 
